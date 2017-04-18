f85e10f8ec3c5ab085fbaf875dc8f2d62c3ca915
Simon Mitchell <simon@3sidedcube.com>
2017-04-18 12:00:43 +0100

    Adds basic configuration method to module

--- ios/RNWorldPay.m
+++ ios/RNWorldPay.m
@@ -1,4 +1,13 @@
 #import "RNWorldPay.h"
+#import "Worldpay.h"
+#import <React/RCTConvert.h>
+
+@implementation RCTConvert (WorldPay)
+RCT_ENUM_CONVERTER(WorldpayValidationType, (@{ @"advanced" : @(WorldpayValidationTypeAdvanced),
+                                               @"basic" : @(WorldpayValidationTypeBasic)
+                                            }),WorldpayValidationTypeBasic, intValue);
+@end
+
 
 @implementation RNWorldPay
 
@@ -6,7 +15,21 @@
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(configure:(id)config) {
+    
+    [[Worldpay sharedInstance] setClientKey:config[@"clientKey"]];
+    
+    if (config[@"reusable"] && [config[@"reusable"] isKindOfClass:[NSNumber class]]) {
+        [[Worldpay sharedInstance] setReusable:[RCTConvert BOOL:config[@"reusable"]]];
+    }
+    
+    if (config[@"validation"] && [config[@"validation"] isKindOfClass:[NSString class]]) {
+        [[Worldpay sharedInstance] setValidationType:[RCTConvert WorldpayValidationType:config[@"validation"]]];
+    }
+}
 
 @end
   
