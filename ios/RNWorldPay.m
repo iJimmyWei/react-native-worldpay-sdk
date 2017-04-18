a66f6cd464ee89c4d17d7fa9e419416108d6ad2a
Simon Mitchell <simon@3sidedcube.com>
2017-04-18 13:23:47 +0100

    Adds methods for interacting with all iOS SDK methods other than UI methods

--- ios/RNWorldPay.m
+++ ios/RNWorldPay.m
@@ -31,5 +31,37 @@ RCT_EXPORT_METHOD(configure:(id)config) {
     }
 }
 
+RCT_EXPORT_METHOD(createToken:(id)cardInfo resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    
+    [[Worldpay sharedInstance] createTokenWithNameOnCard:cardInfo[@"name"] cardNumber:cardInfo[@"number"] expirationMonth:cardInfo[@"expiryMonth"] expirationYear:cardInfo[@"expiryYear"] CVC:cardInfo[@"cvc"] success:^(int code, NSDictionary *responseDictionary) {
+        resolve(@{@"code":@(code), @"response": responseDictionary});
+    } failure:^(NSDictionary *responseDictionary, NSArray *errors) {
+        reject(@"CREATE_TOKEN_FAILED", errors.firstObject ? ((NSError *)errors.firstObject).localizedDescription : @"Unknown Error", errors.firstObject);
+    }];
+}
+
+RCT_EXPORT_METHOD(reuseToken:(NSString *)token cvc:(NSString *)cvc resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject){
+    
+    [[Worldpay sharedInstance] reuseToken:token withCVC:cvc success:^(int code, NSDictionary *responseDictionary) {
+        resolve(@{@"code":@(code), @"response": responseDictionary});
+    } failure:^(NSDictionary *responseDictionary, NSArray *errors) {
+        reject(@"REUSE_TOKEN_FAILED", errors.firstObject ? ((NSError *)errors.firstObject).localizedDescription : @"Unknown Error", errors.firstObject);
+    }];
+}
+
+RCT_EXPORT_METHOD(validateCardDetails:(id)cardInfo resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    
+    NSArray<NSError *> *errors = [[Worldpay sharedInstance] validateCardDetailsWithHolderName:cardInfo[@"name"] cardNumber:cardInfo[@"number"] expirationMonth:cardInfo[@"expiryMonth"] expirationYear:cardInfo[@"expiryYear"] CVC:cardInfo[@"cvc"]];
+    resolve(@{@"errors": errors});
+}
+
+RCT_EXPORT_METHOD(validateCVC:(NSString *)cvc cardToken:(NSString *)cardToken resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    NSArray<NSError *> *errors = [[Worldpay sharedInstance] validateCardDetailsWithCVC:cvc token:cardToken];
+    resolve(@{@"errors": errors});
+}
+
 @end
   
